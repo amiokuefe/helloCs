c02e8394a9a6611f0e01f4c30f39977cea4b26b6
amiokuefe <amiokuefe01@gmail.com>
2022-02-03 15:07:26 +0100

    Add project files.

---
+++ helloCs/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+string firstName = "Efe ";
+string lastName = "Amioku";
+
+int fNum = 93;
+int sNum = 200;
+
+int aNum = sNum - fNum;
+
+int bNum = 14;
+
+double cNum = bNum;
+
+double dNum = 9.94;
+
+int eNum = (int)dNum;
+
+
+string fullName = firstName + lastName;
+/*
+Console.WriteLine(fullName + " " + aNum);
+Console.WriteLine(cNum);
+
+Console.WriteLine(eNum);
+
+Console.WriteLine(Convert.ToString(cNum) + " Hello There"); */
+
+Console.WriteLine(fullName.IndexOf("A"));
+
+int lastNameIndex = fullName.IndexOf("A");
+
+Console.WriteLine(fullName.Substring(lastNameIndex));
+
+/*
+Console.WriteLine("Please enter your name: ");
+
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello " + userName);
+
+Console.WriteLine("Please enter your age: ");
+
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(age); */
+
